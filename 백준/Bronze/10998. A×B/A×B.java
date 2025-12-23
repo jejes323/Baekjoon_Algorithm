c4c8f193be9ddf23c63a4910b7ab49f55b912445
jejes323 <jejes323@gmail.com>
2025-12-23 15:06:05 +0900

    [Bronze V] Title: A×B, Time: 100 ms, Memory: 14260 KB -BaekjoonHub

---
+++ 백준/Bronze/10998. A×B/A×B.java
@@ -0,0 +1,12 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] arg) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] str = br.readLine().split(" ");
+        int a = Integer.parseInt(str[0]);
+        int b = Integer.parseInt(str[1]);
+        
+        System.out.println(a*b);
+    }
+}
