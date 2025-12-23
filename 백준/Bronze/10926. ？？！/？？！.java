165945a4907065a1db243e820da58f5109ce3d5b
jejes323 <jejes323@gmail.com>
2025-12-23 15:33:45 +0900

    [Bronze V] Title: ??!, Time: 104 ms, Memory: 14244 KB -BaekjoonHub

---
+++ 백준/Bronze/10926. ？？！/？？！.java
@@ -0,0 +1,10 @@
+import java.io.*;
+
+public class Main{
+    public static void main(String[] arg) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String str = br.readLine();
+        System.out.println(str + "??!");
+        
+    }
+}
