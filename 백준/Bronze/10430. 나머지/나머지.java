103cdc195c11829fabc8698d230ea8ccc799a934
jejes323 <jejes323@gmail.com>
2025-12-23 15:50:27 +0900

    [Bronze V] Title: 나머지, Time: 104 ms, Memory: 14172 KB -BaekjoonHub

---
+++ 백준/Bronze/10430. 나머지/나머지.java
@@ -0,0 +1,16 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] arg) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] str = br.readLine().split(" ");
+        int a = Integer.parseInt(str[0]);
+        int b = Integer.parseInt(str[1]);
+        int c = Integer.parseInt(str[2]);
+        
+        System.out.println((a+b)%c);
+        System.out.println(((a%c)+(b%c))%c);
+        System.out.println((a*b)%c);
+        System.out.println((a%c)*(b%c)%c);
+    }
+}
