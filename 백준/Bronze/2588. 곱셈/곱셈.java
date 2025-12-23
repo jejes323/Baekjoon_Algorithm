0dceb18934da155fc2974dfd09239d3c674dd3b3
jejes323 <jejes323@gmail.com>
2025-12-23 16:08:29 +0900

    [Bronze III] Title: 곱셈, Time: 100 ms, Memory: 14080 KB -BaekjoonHub

---
+++ 백준/Bronze/2588. 곱셈/곱셈.java
@@ -0,0 +1,18 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] arg) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int a = Integer.parseInt(br.readLine());
+        int b = Integer.parseInt(br.readLine());
+        
+        int b1 = (b % 100) % 10;
+        int b2 = ((b % 100) / 10);
+        int b3 = (b / 100);
+        
+        System.out.println(a*b1);
+        System.out.println(a*b2);
+        System.out.println(a*b3);
+        System.out.println(a*b);;
+    }
+}
