37129c936205c336094bc4083416c640bf57e8f8
jejes323 <jejes323@gmail.com>
2025-12-26 00:50:31 +0900

    [Bronze V] Title: 개수 세기, Time: 100 ms, Memory: 14284 KB -BaekjoonHub

---
+++ 백준/Bronze/10807. 개수 세기/개수 세기.java
@@ -0,0 +1,23 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+        int N = Integer.parseInt(br.readLine());
+        String[] str = br.readLine().split(" ");
+        int v = Integer.parseInt(br.readLine());
+        
+        int count = 0;
+        
+        for(int i =0; i < N; i++) {
+            if(Integer.parseInt(str[i]) == v) {
+                count++;
+            }
+        }
+        System.out.println(count);
+        
+        
+    }
+}
