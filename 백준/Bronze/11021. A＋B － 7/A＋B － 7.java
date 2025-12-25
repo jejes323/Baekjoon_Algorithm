976f278c564f462c65d5b40bdca8a45070936f32
jejes323 <jejes323@gmail.com>
2025-12-25 16:55:47 +0900

    [Bronze V] Title: A+B - 7, Time: 128 ms, Memory: 16028 KB -BaekjoonHub

---
+++ 백준/Bronze/11021. A＋B － 7/A＋B － 7.java
@@ -0,0 +1,16 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int tcase = Integer.parseInt(br.readLine());
+        
+        for(int i = 1; i <= tcase; i++) {
+            String[] str = br.readLine().split(" ");
+            int a = Integer.parseInt(str[0]);
+            int b = Integer.parseInt(str[1]);
+            int sum = a+b;
+            System.out.println("Case #" + i + ": " + sum);
+        }
+    }
+}
