4dd65a4b52f5f23fa70587656411dcf09a96e2c1
jejes323 <jejes323@gmail.com>
2025-12-25 16:58:33 +0900

    [Bronze V] Title: A+B - 8, Time: 148 ms, Memory: 16416 KB -BaekjoonHub

--- 백준/Bronze/11022. A＋B － 8/A＋B － 8.java
+++ 백준/Bronze/11022. A＋B － 8/A＋B － 8.java
@@ -1,27 +1,16 @@
-import java.util.*;
+import java.io.*;
 
 public class Main {
-
-	public static void main(String[] args) {
-		
-		Scanner input = new Scanner(System.in);
-		
-		int tcase = input.nextInt();
-		int[] a = new int[tcase];
-		int[] b = new int[tcase];
-		int[] sum = new int[tcase];
-		
-		for(int i =0; i < tcase; i++) {
-			a[i] = input.nextInt();
-			b[i] = input.nextInt();
-			sum[i] = a[i] + b[i];
-		}
-		
-		for(int i = 0; i < tcase; i++) {
-			System.out.printf("Case #%d: %d + %d = %d\n", i + 1, a[i], b[i], sum[i]);
-		}
-		
-		input.close();
-	}
-
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int tcase = Integer.parseInt(br.readLine());
+        
+        for(int i = 1; i <= tcase; i++) {
+            String[] str = br.readLine().split(" ");
+            int a = Integer.parseInt(str[0]);
+            int b = Integer.parseInt(str[1]);
+            int sum = a + b;
+            System.out.println("Case #" + i + ": " + a + " + " + b + " = " + sum);
+        }
+    }
 }
