962ffb94f77eb9ee9792436725d84458957b67c2
jejes323 <jejes323@gmail.com>
2025-12-26 11:42:43 +0900

    [Bronze II] Title: 나머지, Time: 100 ms, Memory: 14124 KB -BaekjoonHub

--- 백준/Bronze/3052. 나머지/나머지.java
+++ 백준/Bronze/3052. 나머지/나머지.java
@@ -4,21 +4,17 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int[] num = new int[10];
-        for(int i = 0; i < 10; i++) {
-            int a = Integer.parseInt(br.readLine());
-            int result = a % 42;
-            for(int j = 0; j < 10; j++) {
-                if (num[j] == result) {
-                    num[j] = 42;
-                }
-            }
-            num[i] = result;
-        }
+        boolean[] num = new boolean[42];
+        
         int count = 0;
-        for(int i = 0; i <10; i++) {
-            if(num[i] != 42) count++;
+        for(int i = 0; i < 10; i ++) {
+            int a = Integer.parseInt(br.readLine());
+            int target_num = a % 42;
+            if(!num[target_num]) {
+                num[target_num] = true;
+                count++;
+            }
         }
-        System.out.print(count);
+        System.out.println(count);
     }
 }
