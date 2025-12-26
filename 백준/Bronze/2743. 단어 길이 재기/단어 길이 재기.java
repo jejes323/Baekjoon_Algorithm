fbae9bfac79252197b73bb009d01468dd0976f26
jejes323 <jejes323@gmail.com>
2025-12-26 18:56:34 +0900

    [Bronze V] Title: 단어 길이 재기, Time: 104 ms, Memory: 14236 KB -BaekjoonHub

--- 백준/Bronze/2743. 단어 길이 재기/단어 길이 재기.java
+++ 백준/Bronze/2743. 단어 길이 재기/단어 길이 재기.java
@@ -4,7 +4,6 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String str = br.readLine();
-        System.out.print(str.length());
+        System.out.print(br.readLine().length());
     }
 }
