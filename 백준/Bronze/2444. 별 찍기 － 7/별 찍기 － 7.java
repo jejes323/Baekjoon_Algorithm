244f5edb602471e7427fb77c941384a628576ca3
jejes323 <jejes323@gmail.com>
2025-12-26 23:51:57 +0900

    [Bronze III] Title: 별 찍기 - 7, Time: 108 ms, Memory: 14396 KB -BaekjoonHub

--- 백준/Bronze/2444. 별 찍기 － 7/별 찍기 － 7.java
+++ 백준/Bronze/2444. 별 찍기 － 7/별 찍기 － 7.java
@@ -3,29 +3,31 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
+        StringBuilder sb = new StringBuilder();
         for (int i = 0; i < N; i++) {
             for (int j = N - 1; j > i; j--) {
-                System.out.print(" ");
+                sb.append(" ");
             }
             for (int j = i + 1; j > 0; j--) {
-                System.out.print("*");
+                sb.append("*");
             }
             for (int j = i; j > 0; j--) {
-                System.out.print("*");
+                sb.append("*");
             }
-            System.out.print("\n");
+            sb.append("\n");
         }
         for(int i = 0; i < N; i++) {
             for(int j = i + 1; j > 0; j--) {
-                System.out.print(" ");
+                sb.append(" ");
             }
             for(int j = N - 1; j > i; j--) {
-                System.out.print("*");
+                sb.append("*");
             }
             for(int j = N - i - 2; j > 0; j--) {
-                System.out.print("*");
+                sb.append("*");
             }
-            System.out.print("\n");
+            sb.append("\n");
         }
+        System.out.print(sb);
     }
 }
