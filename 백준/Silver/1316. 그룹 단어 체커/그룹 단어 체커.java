20e1c26797d120e0dd347753f50b98370106c0fc
jejes323 <jejes323@gmail.com>
2025-12-29 09:59:33 +0900

    [Silver V] Title: 그룹 단어 체커, Time: 104 ms, Memory: 14224 KB -BaekjoonHub

--- 백준/Silver/1316. 그룹 단어 체커/그룹 단어 체커.java
+++ 백준/Silver/1316. 그룹 단어 체커/그룹 단어 체커.java
@@ -1,6 +1,5 @@
 import java.io.*;
 import java.util.*;
-
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -9,21 +8,18 @@ public class Main {
         int count = 0;
         for(int i = 0; i < N; i++) {
             String st = br.readLine();
-            boolean[] alpha = new boolean[26];
+            boolean num[] = new boolean[26];
             boolean check = true;
-            
             for(int j = 1; j < st.length(); j++) {
-                if(st.charAt(j) != st.charAt(j-1)) {
-                    alpha[(st.charAt(j-1)) - 'a'] = true;
-                    if(alpha[(st.charAt(j) - 'a')]) {
+                if (st.charAt(j) != st.charAt(j-1)) {
+                    num[(st.charAt(j-1)) - 'a'] = true;
+                    if(num[st.charAt(j) - 'a']) {
                         check = false;
                         break;
                     }
                 }
             }
-            if(check) {
-                count++;
-            }
+            if(check) count++;
         }
         System.out.print(count);
     }
