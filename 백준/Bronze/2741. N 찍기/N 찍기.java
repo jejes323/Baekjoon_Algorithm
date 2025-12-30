8253c97a7323fa4fb8c3c64abf2fde37479e7128
jejes323 <jejes323@gmail.com>
2025-12-31 01:41:15 +0900

    [Bronze V] Title: N 찍기, Time: 196 ms, Memory: 18240 KB -BaekjoonHub

---
+++ 백준/Bronze/2741. N 찍기/N 찍기.java
@@ -0,0 +1,14 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int a = Integer.parseInt(br.readLine());
+        StringBuilder sb = new StringBuilder();
+        for(int i = 1; i <= a; i++) {
+            sb.append(i).append('\n');
+        }
+        System.out.print(sb);
+    }
+}
