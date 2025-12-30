621a4d98b1335e3226ae9d719df2053f0b151006
jejes323 <jejes323@gmail.com>
2025-12-31 01:34:12 +0900

    [Bronze I] Title: 소인수분해, Time: 104 ms, Memory: 14308 KB -BaekjoonHub

--- 백준/Bronze/11653. 소인수분해/소인수분해.java
+++ 백준/Bronze/11653. 소인수분해/소인수분해.java
@@ -6,12 +6,14 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
         StringBuilder sb = new StringBuilder();
-        for(int i = 2; i <= n; i++) {
-            if(n % i == 0) {
+        for(int i = 2; i*i <= n; i++) {
+            while(n % i == 0) {
                 sb.append(i).append("\n");
                 n = n / i;
-                i = 1;
             }
+        }
+        if(n != 1) { 
+            sb.append(n);
         }
         System.out.print(sb);
     }
