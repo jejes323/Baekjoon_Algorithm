e4c3bcca41a899b176cf238f5e9aef63d17f958a
jejes323 <jejes323@gmail.com>
2025-12-31 02:44:34 +0900

    [Bronze V] Title: 검증수, Time: 96 ms, Memory: 14212 KB -BaekjoonHub

---
+++ 백준/Bronze/2475. 검증수/검증수.java
@@ -0,0 +1,15 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int sum = 0;
+        for(int i = 0; i < 5; i++) {
+            int a = Integer.parseInt(st.nextToken());
+            sum += a * a;
+        }
+        System.out.print(sum % 10);
+    }
+}
