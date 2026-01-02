8483a7a20dfad02824014becef6b6c7640956054
jejes323 <jejes323@gmail.com>
2026-01-03 01:09:31 +0900

    [Silver V] Title: 영화감독 숌, Time: 104 ms, Memory: 14272 KB -BaekjoonHub

--- 백준/Silver/1436. 영화감독 숌/영화감독 숌.java
+++ 백준/Silver/1436. 영화감독 숌/영화감독 숌.java
@@ -1,28 +1,62 @@
 import java.io.*;
 import java.util.*;
 
+
+
 public class Main {
+    static void solve(int n) {
+        int ret;
+        int i = 0;
+        for(int prev=0; true; prev++) {
+            if(prev % 10 != 6) {
+                i++;
+                ret = prev * 1000;
+                ret += 666;
+                if(i == n) {
+                    System.out.print(ret);
+                    return;
+                }
+            } else if (prev % 100 / 10 != 6) {
+                for(int post = 0; post < 10; post++) {
+                    i++;
+                    ret = prev * 1000;
+                    ret += 660;
+                    ret += post;
+                    if(i == n) {
+                        System.out.print(ret);
+                        return;
+                    }
+                }
+            } else if(prev % 1000 / 100 !=6 ) {
+                for(int post = 0; post < 100; post++) {
+                    i++;
+                    ret = 1000 * prev;
+                    ret += 600;
+                    ret += post;
+                    if(i == n ) {
+                        System.out.print(ret);
+                        return;
+                    }
+                }
+            } else if(prev % 10000/ 1000 != 6) {
+                for(int post = 0; post < 1000; post++) {
+                    i++;
+                    ret = 1000 * prev;
+                    ret += post;
+                    if(i==n) {
+                        System.out.print(ret);
+                        return;
+                    }
+                }
+            }
+        }
+    }
+        
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         
         int tcase = Integer.parseInt(br.readLine());
-        int target = 665;
-        for(int i = 0; i < tcase; i++) {
-            while(true) {
-                target++;
-                String len = String.valueOf(target);
-                boolean check = false;
-                for(int j = 0; j < len.length() - 2; j++) {
-                    if (len.charAt(j) == '6' && len.charAt(j + 1) == '6' && len.charAt(j + 2) == '6') {
-                        check = true;
-                        break;
-                    }
-                }
-                if(check) {
-                    break;
-                }
-            }
-        }
-        System.out.print(target);
+        solve(tcase);
+       
     }
 }
