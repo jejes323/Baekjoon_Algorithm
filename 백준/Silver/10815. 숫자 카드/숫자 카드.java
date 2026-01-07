ebb9b7c42c4836980a879f3215851962ff70bc92
jejes323 <jejes323@gmail.com>
2026-01-07 18:07:22 +0900

    [Silver V] Title: 숫자 카드, Time: 1204 ms, Memory: 109520 KB -BaekjoonHub

---
+++ 백준/Silver/10815. 숫자 카드/숫자 카드.java
@@ -0,0 +1,45 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static boolean check(int[] arr, int target) {
+        int left = 0;
+        int right = arr.length - 1;
+        
+        while(left <= right) {
+            int mid = (left + right) / 2;
+            if(arr[mid] == target) return true;
+            else if(arr[mid] < target) left = mid + 1;
+            else right = mid - 1;
+        }
+        
+        return false;
+    }
+    
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int tcase1 = Integer.parseInt(br.readLine());
+        int[] arr1 = new int[tcase1];
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for(int i = 0; i < tcase1; i++) {
+            arr1[i] = Integer.parseInt(st.nextToken());
+        }
+        
+        int tcase2 = Integer.parseInt(br.readLine());
+        int[] arr2 = new int[tcase2];
+        st = new StringTokenizer(br.readLine());
+        for(int i = 0; i < tcase2; i++) {
+            arr2[i] = Integer.parseInt(st.nextToken());
+        }
+        
+        Arrays.sort(arr1);
+        
+        StringBuilder sb = new StringBuilder();
+        for(int x : arr2) {
+            if(check(arr1, x)) sb.append(1).append('\n');
+            else sb.append(0).append('\n');
+        }
+        
+        System.out.print(sb);
+    }
+}
