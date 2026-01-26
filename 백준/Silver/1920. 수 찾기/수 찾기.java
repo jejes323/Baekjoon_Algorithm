e98503ad05419581fdf9030a507c831fe73414c9
jejes323 <jejes323@gmail.com>
2026-01-27 02:26:53 +0900

    [Silver IV] Title: 수 찾기, Time: 540 ms, Memory: 41588 KB -BaekjoonHub

--- 백준/Silver/1920. 수 찾기/수 찾기.java
+++ 백준/Silver/1920. 수 찾기/수 찾기.java
@@ -2,47 +2,40 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    public static boolean check(int[] arr, int target) {
+    static int[] arr;
+    public static int check(int target) {
         int left = 0;
         int right = arr.length - 1;
         
         while(left <= right) {
             int mid = (left + right) / 2;
-            if(arr[mid] == target) return true;
+            if(arr[mid] == target) return 1;
             else if(arr[mid] < target) left = mid + 1;
             else right = mid - 1;
         }
         
-        return false;
+        return 0;
     }
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st;
-        int tcase1 = Integer.parseInt(br.readLine());
-        int[] arr1 = new int[tcase1];
-        st = new StringTokenizer(br.readLine());
+        int a = Integer.parseInt(br.readLine());
+        arr = new int[a];
         
-        for(int i = 0; i < tcase1; i++) {
-            arr1[i] = Integer.parseInt(st.nextToken());
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for(int i = 0; i < a; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
         }
         
-        int tcase2 = Integer.parseInt(br.readLine());
-        int[] arr2 = new int[tcase2];
+        Arrays.sort(arr);
+        int b = Integer.parseInt(br.readLine());
         st = new StringTokenizer(br.readLine());
-        
-        for(int i = 0; i < tcase2; i++) {
-            arr2[i] = Integer.parseInt(st.nextToken());
-        }
-        
-        Arrays.sort(arr1);
         
         StringBuilder sb = new StringBuilder();
-        for(int x : arr2) {
-            if(check(arr1, x)) sb.append(1).append('\n');
-            else sb.append(0).append('\n');
+        for(int i = 0; i < b; i++) {
+            int x = Integer.parseInt(st.nextToken());
+            sb.append(check(x)).append('\n');
         }
-        
-        System.out.print(sb);
-        
+        System.out.println(sb);
     }
 }
