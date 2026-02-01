8f07d46da470cd02f8e7d7f2cb52b294f45f56a0
jejes323 <jejes323@gmail.com>
2026-02-02 02:04:15 +0900

    [Silver IV] Title: 요세푸스 문제 0, Time: 104 ms, Memory: 14376 KB -BaekjoonHub

--- 백준/Silver/11866. 요세푸스 문제 0/요세푸스 문제 0.java
+++ 백준/Silver/11866. 요세푸스 문제 0/요세푸스 문제 0.java
@@ -5,28 +5,28 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        int n = Integer.parseInt(st.nextToken());
-        int k = Integer.parseInt(st.nextToken());
+        
+        int N = Integer.parseInt(st.nextToken());
+        int target = Integer.parseInt(st.nextToken());
         
         Deque<Integer> dq = new ArrayDeque<>();
-       
-        
-        for(int i = 1; i <= n; i++) {
+        for(int i = 1; i <= N; i++) {
             dq.addLast(i);
         }
         
         StringBuilder sb = new StringBuilder();
-        sb.append("<");
-        
-      
-        while(dq.size() != 1) {
-            for(int i = 0; i < k - 1; i++) {
+        sb.append('<');
+        while(dq.size() != 0) {
+            
+            for(int i = 0; i < target - 1; i++) {
                 dq.addLast(dq.pollFirst());
             }
-            sb.append(dq.pollFirst()).append(", ");
+            if(dq.size() == 1) sb.append(dq.pollFirst());
+            else sb.append(dq.pollFirst()).append(", ");
+            
         }
-        sb.append(dq.pollFirst()).append(">");
+        sb.append('>');
         
-        System.out.print(sb);
+        System.out.println(sb);
     }
 }
