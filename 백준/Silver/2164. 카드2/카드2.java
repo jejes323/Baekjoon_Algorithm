f5c42d93cd15f4b4da91726079c43a15440b0ef0
jejes323 <jejes323@gmail.com>
2026-02-02 01:43:39 +0900

    [Silver IV] Title: 카드2, Time: 164 ms, Memory: 28904 KB -BaekjoonHub

--- 백준/Silver/2164. 카드2/카드2.java
+++ 백준/Silver/2164. 카드2/카드2.java
@@ -2,21 +2,22 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
+   public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int n = Integer.parseInt(br.readLine());
+        int a = Integer.parseInt(br.readLine());
         
         Deque<Integer> dq = new ArrayDeque<>();
-        
-        for(int i = 1; i <= n; i++) {
+        for(int i = 1 ; i <= a; i++) {
             dq.addLast(i);
         }
+       
         
         while(dq.size() != 1) {
             dq.pollFirst();
-            dq.addLast(dq.pollFirst());
+            
+            if(dq.size() != 1) dq.addLast(dq.pollFirst());
         }
         
-        System.out.print(dq.peekFirst());
+        System.out.println(dq.peekFirst());
     }
 }
