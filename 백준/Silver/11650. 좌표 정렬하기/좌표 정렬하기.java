c1391d4b9f0b74033dc1524533b2a8bc32e3d0a4
jejes323 <jejes323@gmail.com>
2026-02-07 01:24:53 +0900

    [Silver V] Title: 좌표 정렬하기, Time: 520 ms, Memory: 50708 KB -BaekjoonHub

--- 백준/Silver/11650. 좌표 정렬하기/좌표 정렬하기.java
+++ 백준/Silver/11650. 좌표 정렬하기/좌표 정렬하기.java
@@ -2,20 +2,18 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
+        public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int tcase = Integer.parseInt(br.readLine());
-        StringTokenizer st;
         
         int[][] arr = new int[tcase][2];
         
         for(int i = 0; i < tcase; i++) {
-            st = new StringTokenizer(br.readLine());
-            int x = Integer.parseInt(st.nextToken());
-            int y = Integer.parseInt(st.nextToken());
-            arr[i][0] = x;
-            arr[i][1] = y;
-            
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            arr[i][0] = a;
+            arr[i][1] = b;
         }
         
         Arrays.sort(arr, (a, b) -> {
@@ -24,10 +22,9 @@ public class Main {
         });
         
         StringBuilder sb = new StringBuilder();
-        for(int i = 0 ; i < tcase; i++) {
+        for(int i = 0; i < tcase;i ++) {
             sb.append(arr[i][0]).append(" ").append(arr[i][1]).append('\n');
         }
-        System.out.print(sb);
-        
+        System.out.println(sb);
     }
 }
