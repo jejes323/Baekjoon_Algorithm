5e5e6d395936e8a8303333841e84e7e9d245f809
jejes323 <jejes323@gmail.com>
2026-02-25 22:40:00 +0900

    [Silver I] Title: 절댓값 힙, Time: 300 ms, Memory: 26064 KB -BaekjoonHub

---
+++ 백준/Silver/11286. 절댓값 힙/절댓값 힙.java
@@ -0,0 +1,31 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+        public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        
+        PriorityQueue<Integer> pq = new PriorityQueue<>((o1, o2) -> {
+            int first = Math.abs(o1);
+            int second = Math.abs(o2);
+            
+            if(first == second) return o1 > o2 ? 1 : -1;
+            return first - second;
+        });
+        
+        StringBuilder sb = new StringBuilder();
+        for(int i = 0; i < N; i++) {
+            int x = Integer.parseInt(br.readLine());
+            
+            if(x == 0) {
+                if(pq.isEmpty()) sb.append("0").append('\n');
+                else sb.append(pq.poll()).append('\n');
+            }
+            
+            else pq.offer(x);
+        }
+        
+        System.out.println(sb);
+    }
+}
